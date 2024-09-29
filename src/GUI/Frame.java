583d9f51b5f6d29ad9c27b6608840a513bd595e1
Matthes Böttcher <matthesboettcher03@gmail.com>
2024-09-29 09:18:51 +0200

    Aktualisierungen durch veraenderten Code

--- src/GUI/Frame.java
+++ src/GUI/Frame.java
@@ -1,14 +1,11 @@
 package GUI;
 
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 
 public class Frame extends JFrame {
     public Frame() {
         super("Taschenrechner");
-        JLabel label = new JLabel("Geeks Premier League 2023");
-        this.add(label);
-        this.setSize(700,700);
+        this.setSize(600,600);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
