e317debe9890f5ae43c7164e006c410944a50d2c
Matthes Böttcher <matthesboettcher03@gmail.com>
2024-09-29 07:59:34 +0200

    Main erstellt JFrame und Object vom Screen

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+import GUI.Frame;
+import GUI.Screen;
+
+public class Main {
+    public static void main(String[] Args) {
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new Frame();
+
+            Screen screen = new Screen(frame);
+            screen.setButtons();
+        });
+    }
+}
