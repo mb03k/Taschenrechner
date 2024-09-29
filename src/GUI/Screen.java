5243014c9616fde48fa87257854efe8b0530f689
Matthes Böttcher <matthesboettcher03@gmail.com>
2024-09-29 07:59:13 +0200

    JFrame anzeigen lassen

---
+++ src/GUI/Screen.java
@@ -0,0 +1,20 @@
+package GUI;
+
+import javax.swing.JFrame;
+
+public class Screen {
+    private JFrame frame;
+
+    public Screen(JFrame frame) {
+        this.frame = frame;
+        frame.setVisible(true);
+    }
+
+    public void setButtons() {
+        setGridLayout();
+    }
+
+    public void setGridLayout() {
+
+    }
+}
